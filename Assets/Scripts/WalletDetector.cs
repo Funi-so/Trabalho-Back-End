4ac965d0e77e7e5e91572be33f841b7db86183fc
Big Craudio <claudio.ramos@sga.pucminas.br>
2025-06-22 16:46:47 -0300

    Codigo quase pronto

--- Assets/Scripts/WalletDetector.cs
+++ Assets/Scripts/WalletDetector.cs
@@ -10,31 +10,90 @@ public class WalletDetector : MonoBehaviour
 {
     public int wallet;
     public int price;
+    public GameObject Arvore;
+    public GameObject banana;
+    public GameObject Chapeu;
     public TMP_Text txtlabel;
     [SerializeField] private String url;
 
-    void Start()
+
+
+    public void ComprarArvore()
     {
-        UpdateWallet();
+        StartCoroutine("BuyArvore");
     }
 
-    // Update is called once per frame
-    public void UpdateWallet()
+    public void Comprarbanana()
     {
-        StartCoroutine("LoadText");
+        StartCoroutine("Buybanana");
     }
 
+    public void ComprarChapeu()
+    {
+        {
+            StartCoroutine("BuyHat");
+        }
 
-    IEnumerator LoadText()
+       
+    }
+    IEnumerator Buybanana()
     {
         UnityWebRequest request = UnityWebRequest.Get(url);
         yield return request.SendWebRequest();
         if (request.result == UnityWebRequest.Result.Success)
         {
             txtlabel.text = request.downloadHandler.text;
-            
+
             price = int.Parse(txtlabel.text);
+            if (wallet >= price)
+            {
+                wallet = -price;
+                banana.SetActive(true);
+                txtlabel.text = "você comprou a banana";
+            }
+            txtlabel.text = "você n tem dinheiro para comprar isso";
         }
-        
+
+    }
+    IEnumerator BuyHat()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            txtlabel.text = request.downloadHandler.text;
+
+            price = int.Parse(txtlabel.text);
+            if (wallet >= price)
+            {
+                wallet = -price;
+                Chapeu.SetActive(true);
+                txtlabel.text = "você comprou o chapeu";
+            }
+            txtlabel.text = "você n tem dinheiro para comprar isso";
+        }
+
+
+
+    }
+    IEnumerator BuyArvore()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            txtlabel.text = request.downloadHandler.text;
+
+            price = int.Parse(txtlabel.text);
+            if (wallet >= price)
+            {
+                wallet = -price;
+                Arvore.SetActive(true);
+                txtlabel.text = "você comprou a arvore";
+            }
+            txtlabel.text = "você n tem dinheiro para comprar isso";
+        }
+
     }
 }
+
