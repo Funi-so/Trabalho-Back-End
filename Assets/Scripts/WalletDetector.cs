7990c15545b6b57405d10fe39fc8c9a3ec807084
Big Craudio <claudio.ramos@sga.pucminas.br>
2025-06-22 15:30:14 -0300

    Codigo fodah
    Esse codigo supostamente vai pegar o valor de cada item pra fazer o calculo se o jogador pode comprar ou n

---
+++ Assets/Scripts/WalletDetector.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+
+public class WalletDetector : MonoBehaviour
+{
+    public int wallet;
+    public int price;
+    public TMP_Text txtlabel;
+    [SerializeField] private String url;
+
+    void Start()
+    {
+        UpdateWallet();
+    }
+
+    // Update is called once per frame
+    public void UpdateWallet()
+    {
+        StartCoroutine("LoadText");
+    }
+
+    IEnumerator LoadText()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            txtlabel.text = request.downloadHandler.text;
+            int.TryParse(txtlabel.text, out price);
+        }
+        
+    }
+}
